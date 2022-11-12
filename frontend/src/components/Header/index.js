ce951d6d9dc7f3141536ee66828f9eed71c6ff1b
sriharshasaswath <saswath929@gmail.com>
2022-11-13 00:00:30 +0530

    small changes

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,11 +1,22 @@
+import { AiOutlineVideoCamera } from "react-icons/ai";
 import "./index.css";
 
 const Header = () => {
   return (
     <div className="Header">
-      <h1>
-        <strong>Virtual Cards</strong>
-      </h1>
+      <div className="container">
+        <h1>
+          <strong>Virtual Cards</strong>
+        </h1>
+        <AiOutlineVideoCamera className="camera" />
+        <p
+          style={{
+            color: "blue",
+          }}
+        >
+          Learn more
+        </p>
+      </div>
       <p className="card-holder">+ Virtual cards</p>
     </div>
   );
